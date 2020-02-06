7fc80ea1850374140c5a17c70b994027eb198c8e
Commoddity <pascalvanleeuwen604@gmail.com>
2020-02-06 04:00:59 +0000

    Added admin dashboard with links to categories and products pages.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_LOGIN'], :password => ENV['ADMIN_PASSWORD']
 
   def show
+    @categories = Category.all 
+    @products = Product.all
   end
+
 end
