f20ff03cde32916b6e6bd9ff4da14e8a8ca93946
Commoddity <pascalvanleeuwen604@gmail.com>
2020-02-11 03:42:35 +0000

    Completed up to Step 2 of Model spec: user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+  before_save { |user| user.email = email.downcase }
   
+  validates :email, :presence => true, :uniqueness => true
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :password, :confirmation => true, length: { minimum: 8 }
+  validates :password_confirmation, :presence => true
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    subject {
+      User.new( email: "mike@dogs.com", 
+                first_name: "Mike", 
+                last_name: "Dogs", 
+                password: "dogsrule", 
+                password_confirmation: "dogsrule")
+    }
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a first_name" do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a last_name" do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a password_confirmation" do
+      subject.password_confirmation = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid if password and password_confirmation do not match" do
+      subject.password = "dogsrule"
+      subject.password_confirmation = "catsrule"
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid if email is not unique" do
+      subject.save
+
+      user2 = User.new( email: "mike@dogs.com", 
+        first_name: "Bob ", 
+        last_name: "Cats", 
+        password: "catsrule", 
+        password_confirmation: "catsrule")
+
+      expect(user2).to_not be_valid
+    end
+
+  end
+end
